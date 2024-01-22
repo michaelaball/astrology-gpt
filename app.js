e00e060361ef2e814dcc52405bf8b55a36efd235
Your Name <you@example.com>
2024-01-21 18:00:58 -0600

    Add method call to get planet in sign for a specific date.

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ import chatgpt from './chatgpt.js';
 import astrology from './astrology.js';
 
 astrology.getPlanetaryAlignments();
+
+astrology.getPlanetInSign('sun', new Date('2015-08-10T17:09:01.000+08:00'));
