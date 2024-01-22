bd305cce56756a8574b974c00775eebb500b21aa
Your Name <you@example.com>
2024-01-21 18:08:13 -0600

    Change default date for planetary alignments to October 6, 1989.

--- astrology.js
+++ astrology.js
@@ -8,7 +8,7 @@ function getSign(longitude) {
 
 export default {
     getPlanetaryAlignments: ()=> {
-    const dateObj = new Date('2015-08-10T17:09:01.000+08:00');
+    const dateObj = new Date('1989-10-06T17:09:01.000+08:00');
  
     // parameters: ephemeris.getAllPlanets(dateObj, longitude, latitude, height);
     var result = ephemeris.getAllPlanets(dateObj, 10.0014, 53.5653, 0);
