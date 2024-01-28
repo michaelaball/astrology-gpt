d6c01b58370cd1578238fa0e76624b138f8adc4b
Your Name <you@example.com>
2024-01-28 15:40:47 -0600

    Refactored astrology.js to export functions and added getYearlyAlignments in app.js.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 import fs from 'fs';
 import chatgpt from './chatgpt.js';
-import astrology from './astrology.js';
+import {getYearlyAlignments} from './astrology.js';
 
 // astrology.getPlanetaryAlignments();
 
 // astrology.getPlanetInSign('venus', new Date('1989-10-06T17:09:01.000+08:00'));
 
 // const birthChart = astrology.getBirthChart(new Date('1989-10-06T17:09:01.000+08:00'));
-const birthChart = astrology.getBirthChart(new Date());
+// const birthChart = astrology.getBirthChart(new Date());
 // console.log('birthChart: ',birthChart);
-const prompt = fs.readFileSync('forecast-prompt.txt', 'utf8').replace('\n', ' ') + `\n${birthChart}`;
+// const prompt = fs.readFileSync('forecast-prompt.txt', 'utf8').replace('\n', ' ') + `\n${birthChart}`;
+
+
 
 //You are to be a leader.  You are to be a prophet.  You are to be a visionary.  You are to be a sage.  You are to be a seer.  You are to be a soothsayer.  You are to be a diviner.  You are to be a forecaster.  You are to be a prognosticator.`
 //  You are to be a fortune teller.  You are to be a clairvoyant.  You are to be a psychic.  You are to be a medium.  You are to be a palm reader.  You are to be a tarot reader.  You are to be a numerologist.  You are to be a graphologist.  You are to be a phrenologist.  You are to be a chiromancer.  You are to be a geomancer.  You are to be a bibliomancer.  You are to be a cartomancer.  You are to be a dowsing practitioner.  You are to be a dream interpreter.  You are to be a crystal gazer.  You are to be a scryer.  You are to be a tea leaf reader.  You are to be a tasseographer.  You are to be a rumpologist.  You are to be a oneiromancer.  You are to be a necromancer.  You are to be a pyromancer.  You are to be a hydromancer.  You are to be a aeromancer.  You are to be a ceromancer.  You are to be a capnomancer.  You are to be a ceraunomancer.  You are to be a gastromancer.  You are to be a haruspex.  You are to be a haruspication practitioner.  You are to be a hepatomancer.  You are to be a lecanomancer.  You are to be a lithomancer.  You are to be a margaritomancer.  You are to be a myomancer.  You are to be a necyomancer.  You are to be a omphalomancer.  You are to be a onychomancer.  You are to be a oomancer.  You are`;
-console.log('prompt: ',prompt);
-chatgpt.chatGptInterpret(prompt).then(response => console.log(response));
+// console.log('prompt: ',prompt);
+// chatgpt.chatGptInterpret(prompt).then(response => console.log(response));
+
+
+const yearly = getYearlyAlignments()
+console.log('yearly: ',yearly)
--- astrology.js
+++ astrology.js
@@ -6,48 +6,71 @@ function getSign(longitude) {
     return signs[Math.floor((longitude - 1) / 30)];
 }
 
-export default {
-    getPlanetaryAlignments: ()=> {
+    export function getPlanetaryAlignments() {
     const dateObj = new Date('1989-10-06T17:09:01.000+08:00');
  
     // parameters: ephemeris.getAllPlanets(dateObj, longitude, latitude, height);
     var result = ephemeris.getAllPlanets(dateObj, 10.0014, 53.5653, 0);
     console.log(result)
-    },
+    }
 
-    getHouses: (dateObj, longitude, latitude) => {
+    export function getHouses(dateObj, longitude, latitude) {
         const houses = ephemeris.getHouses(dateObj, longitude, latitude);
         console.log(houses);
-    },
+    }
 
-    getPlanetInSign: (planet, dateObj) => {
+    export function getPlanetInSign (planet, dateObj) {
         const allPlanets = ephemeris.getAllPlanets(dateObj);
         const planetPosition = allPlanets.observed[planet];
         console.log('planetPosition', planetPosition);
         const sign = getSign(planetPosition.apparentLongitudeDd);
         console.log(`${planet} is in ${sign}`);
-    },
+    }
 
-    getBirthChart: (dateObj) => {
+    export function getBirthChart (dateObj) {
         const allPlanets = ephemeris.getAllPlanets(dateObj);
         let alignments = []
         for (let planet in allPlanets.observed) {
             const planetPosition = allPlanets.observed[planet];
             const sign = getSign(planetPosition.apparentLongitudeDd);
-            alignments.push(`${planet} is in ${sign}`)
+            alignments.push([planet, sign])
         }
-        return alignments.join('\n')
-    },
+        return Object.fromEntries(alignments)
+    }
 
-    getYearlyAlignments: () => {
-        let alignments = {};
+    export function getYearlyAlignments() {
+        let alignments = [];
         let dateObj = new Date();
         for (let i = 0; i < 365; i++) {
             let dateStr = dateObj.toISOString().split('T')[0].replace(/-/g, '');
-            alignments[dateStr] = getBirthChart(dateObj);
+            alignments.push([dateStr, getBirthChart(dateObj)])
             dateObj.setDate(dateObj.getDate() + 1);
         }
         return alignments;
     }
-}
+
+    export function getYearlyAlignmentAdvanced() {
+        const basic = getYearlyAlignments()
+        const advanced = basic.map(([dateId, alignment]) => {
+            // Fill this in.
+            /* alignment will look like this {
+                'sun': 'Capricorn',
+                'moon: 'Sagittarius',
+                ...etc
+              }
+            We would like to return an advanced alignment like this:
+            {
+                'sun': {
+                    current: 'Capricorn',
+                    until: '20241219',
+                    next: 'Aquarius'
+                },
+                ... etc
+            }
+            So that current is the current sign for that date (as in the original alignment)
+            until will be the date that changes (if present in the rest of basic array)
+            next will be the name of the next sign for the date that it changes (from the basic array)
+            */
+        })
+    }
  
